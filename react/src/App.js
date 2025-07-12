1439b40e64a0cf25257a4809e41090af12f87fe8
IbragimKhaniev <newinterny@gmail.com>
2025-07-12 12:35:01 +0300

    Update files

--- react/src/App.js
+++ react/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import logo from './logo.svg';
 import ErrorBoundary from './ErrorBoundary';
 import Calculator from './components/Calculator';
 import './App.css';
@@ -6,11 +8,21 @@ import './App.css';
 function App() {
   return (
     <ErrorBoundary>
-      <div className="App">
-        <header className="App-header">
-          <Calculator />
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Шаблон React успешно развернут, <br />
+              Ждите обновлений от AI :)
+            </p>
+            <Routes>
+              <Route path="/calculator" element={<Calculator />} />
+              <Route path="/" element={<div>Welcome to the App! Go to <a href="/calculator">Calculator</a></div>} />
+            </Routes>
+          </header>
+        </div>
+      </Router>
     </ErrorBoundary>
   );
 }
